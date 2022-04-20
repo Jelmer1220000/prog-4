ea8ea38b351632285c486550eaa1fd9157e0c751
Jelmer1220000 <jjp.verhoeven@student.avans.nl>
2022-04-20 10:17:00 +0200

    added logger

---
+++ config/config.js
@@ -0,0 +1,25 @@
+const loglevel = process.env.LOGLEVEL || 'trace'
+
+module.exports = {
+  
+  dbconfig: {
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'test',
+    database: process.env.DB_DATABASE || 'studenthome',
+    password: process.env.DB_PASSWORD || 'your_password',
+    multipleStatements: true,
+    connectionLimit: 10
+  },
+
+  jwtSecretKey: process.env.SECRET || 'secret',
+
+  logger: require('tracer').console({
+    format: ['{{timestamp}} [{{title}}] {{file}}:{{line}} : {{message}}'],
+    preprocess: function (data) {
+      data.title = data.title.toUpperCase()
+    },
+    dateformat: 'isoUtcDateTime',
+    level: loglevel
+  })
+  
+}
