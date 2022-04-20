253af295a5d1fa1ac78902b3f758553b181d16e3
Jelmer1220000 <jjp.verhoeven@student.avans.nl>
2022-04-20 10:55:15 +0200

    fixed port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const userroutes = require('./routes/user-routes')
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.writeHead(200, { "Content-Type": "text/json" });
