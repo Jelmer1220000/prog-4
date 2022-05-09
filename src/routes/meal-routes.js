b5e9cff4b67223cd4ff9981d024ce70356ad7581
Jelmer1220000 <jjp.verhoeven@student.avans.nl>
2022-05-09 17:24:21 +0200

    fixed everything

--- src/routes/meal-routes.js
+++ src/routes/meal-routes.js
@@ -3,14 +3,14 @@ const validator = require('../controllers/validator')
 const mealcontroller = require('../controllers/meal-controller')
 const router = express.Router()
 
-router.get('/meal', mealcontroller.getAllMeals)
-router.get('/meal/:mealId', mealcontroller.getMealById)
-router.get('meal/:mealId/participate', mealcontroller.joinMeal)
+router.get('', mealcontroller.getAllMeals)
+router.get('/:mealId', mealcontroller.getMealById)
+router.get('/:mealId/participate', mealcontroller.joinMeal)
 
-router.put('/meal/:mealId', validator.validateMeal, mealcontroller.changeMeal)
+router.put('/:mealId', validator.validateMeal, mealcontroller.changeMeal)
 
-router.post('/meal', validator.validateMeal, mealcontroller.createMeal)
+router.post('', validator.validateMeal, mealcontroller.createMeal)
 
-router.delete('/meal/:mealId', mealcontroller.deleteMeal)
+router.delete('/:mealId', mealcontroller.deleteMeal)
 
 module.exports = router
