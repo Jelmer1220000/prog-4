dd0d3aef97d6cc613666f36d78d7dc304ee7ca14
Jelmer1220000 <jjp.verhoeven@student.avans.nl>
2022-05-10 21:05:26 +0200

    ok done

--- test/integration/user.test.js
+++ test/integration/user.test.js
@@ -90,13 +90,13 @@ describe('User Tests 201-206', () => {
                     city: 'Breda',
                     isActive: 1,
                     roles: '',
-                    emailAdress: 'name#$%@server.nl',
+                    emailAdress: 'name#$#%@server.nl',
                     password: 'secret',
                     phoneNumber: '06-11223344',
                 })
                 .end((err, res) => {
                     assert.ifError(err)
-                    res.should.have.status(409)
+                    res.should.have.status(400)
                     res.should.be.an('object')
 
                     res.body.should.be
